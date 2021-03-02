81fba1a2896ce14a4cd46a0d09d48cd0a72ca5e0
Wenchy Dutreuil <wcdutreuil@gmail.com>
2021-03-02 16:44:59 -0600

    silly extra param to super call

--- src/model/UserSchool.java
+++ src/model/UserSchool.java
@@ -9,11 +9,44 @@ package model;
  */
 public class UserSchool {
 
+	private University university;
+	private User owner;
+	
 	/**
 	 * 
 	 */
-	public UserSchool() {
-		// TODO Auto-generated constructor stub
+	public UserSchool(University university, User owner) {
+		this.university = university;
+		this.owner = owner;
 	}
+
+	/**
+	 * @return the university
+	 */
+	public University getUniversity() {
+		return university;
+	}
+
+	/**
+	 * @param university the university to set
+	 */
+	public void setUniversity(University university) {
+		this.university = university;
+	}
+
+	/**
+	 * @return the owner
+	 */
+	public User getOwner() {
+		return owner;
+	}
+
+	/**
+	 * @param owner the owner to set
+	 */
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+	
 
 }
