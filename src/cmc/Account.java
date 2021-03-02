03a9d346325f95dae44734e5e727f43b40451a73
CKALSOW001 <ckalsow001@vplincsci016.ad.csbsju.edu>
2021-03-02 16:11:05 -0600

    starter code

---
+++ src/cmc/Account.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package cmc;
+
+/**
+ * @author Channa Kalsow
+ *
+ */
+public abstract class Account {
+	protected String firstName, lastName, recoveryQuestion,
+	recoveryAnswer, username, password;
+	protected boolean enabled;
+	
+	public Account(String firstName, String lastName, boolean enabled, String
+			recoveryQuestion, String recoveryAnswer, String username, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.enabled = enabled;
+		this.recoveryQuestion = recoveryQuestion;
+		this.recoveryAnswer = recoveryAnswer;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public boolean logon(String password) {
+		//TODO
+		return true;
+	}
+	public String recover(String answer) {
+		//TODO
+		return " ";
+	}
+	public boolean logout() {
+		//TODO
+		return true;
+	}
+}
---
+++ src/cmc/User.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package cmc;
+
+/**
+ * @author Channa Kalsow
+ *
+ */
+import java.util.List;
+
+public class User extends Account{
+	private List<UserSchool> savedSchools;
+	
+	/**
+	 * 
+	 */
+	public User(String firstName, String lastName, boolean enabled, String
+			recoveryQuestion, String recoveryAnswer, String username,
+			String password, List<UserSchool> savedSchools) {
+		super(firstName);
+		super(lastName);
+		super(enabled);
+		super(recoveryQuestion);
+		super(recoveryAnswer);
+		super(username);
+		super(password);
+		this.savedSchools = savedSchools;
+		
+	}
+
+	public void saveSchool(UserSchool school) {
+		//TODO
+	}
+	
+	public void unsaveSchool(String schoolName) {
+		//TODO
+	}
+	
+	public List getSavedSchools() {
+		return savedSchools;
+	}
+} 
