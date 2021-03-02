8bcac17b38ac057724884919c96aa6f37544ae01
Wenchy Dutreuil <wcdutreuil@gmail.com>
2021-03-02 15:44:01 -0600

    added a PsuedoDatabase class to test functionality of our github setup.

---
+++ src/com/cmc/PsuedoDatabase.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.cmc;
+
+/**
+ * @author wench
+ *
+ */
+public class PsuedoDatabase {
+
+	/**
+	 * 
+	 */
+	public PsuedoDatabase() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public boolean getUsers() {
+		return true;
+	}
+}
