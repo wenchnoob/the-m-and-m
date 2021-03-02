3146c6ef88492e1a47473cb41f10d736171e9e2a
Wenchy Dutreuil <wcdutreuil@gmail.com>
2021-03-02 17:42:20 -0600

    completed a component of the PsuedoDatabase class

--- src/com/cmc/PsuedoDatabase.java
+++ src/com/cmc/PsuedoDatabase.java
@@ -32,32 +32,39 @@ public class PsuedoDatabase {
 	private void initUniversities() {
 		
 	}
-
 	
-	public boolean getUsers() {
-		return true;
-	}
 	public Account getUserbyUsername(String username) {
 		for (Account acc: users) {
-			if (acc.getName)
+			if (acc.getUsername().equals(username)) return acc;
 		}
-		return account; 
+		return null; 
 	}
+	
 	public University findUniversityByName(String name) {
-		return university;
+		for (University uni: universities) {
+			if (uni.getName().equals(name)) return uni;
+		}
+		return null;
 	}
+	
 	public List<Account> getAllUsers() {
 		return users;
 	}
+	
+	
 	public List<University> getAllUniversities() {
 		return universities;
 	}
+	
+	
 	public boolean save(User toAdd) {
 		return users.add(toAdd);
 	}
+	
+	
 	public boolean save(University toAdd) {
 		return universities.add(toAdd);
 	}
 	
-	}
+}
 
