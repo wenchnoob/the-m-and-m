624dc512e29baa85533f3e5811105d35bb9ee773
CKALSOW001 <ckalsow001@vplincsci016.ad.csbsju.edu>
2021-03-02 17:55:59 -0600

    updates

--- src/com/cmc/PsuedoDatabase.java
+++ src/com/cmc/PsuedoDatabase.java
@@ -3,11 +3,13 @@
  */
 package com.cmc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Account;
 import model.University;
 import model.User;
+import model.UserSchool;
 
 /**
  * @author wench
@@ -26,11 +28,25 @@ public class PsuedoDatabase {
 	}
 	
 	private void initUsers() {
-		
+		List<UserSchool> list1 = new ArrayList<UserSchool>();
+		List<UserSchool> list2 = new ArrayList<UserSchool>();
+		User testUser = new User("Channa", "Kalsow", true, "What's your name?",
+				"Channa", "ckalsow", "Channaiskool", list1);
+		User testUser2 = new User("Kristiana", "Anderson", true, "What's Heather's cat's name?",
+				"Ned", "kanderson", "Kristianaiskool", list2);
+		users = new ArrayList<>();
+		users.add(testUser2);
+		users.add(testUser);
 	}
 	
 	private void initUniversities() {
-		
+		List<String> emphases = new List<String>();
+		emphases.add("Space Studies");
+		emphases.add("Puppetry");
+		emphases.add("Galaxy Business");
+		emphases.add("Mars Gardening");
+		University studentsRUs = new University("123 ABC Street", "Mars", "private", 
+				6, 100.0f, 200, 200, 1000000, 0, 500, 1.2f, 1, 5, emphases);
 	}
 
 	
