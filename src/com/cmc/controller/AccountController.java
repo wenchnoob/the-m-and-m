83d751300289ffc45bb332fc2cec0cef81d08c95
Wenchy Dutreuil <wcdutreuil@gmail.com>
2021-03-02 19:01:00 -0600

    fixed some things

--- src/com/cmc/controller/AccountController.java
+++ src/com/cmc/controller/AccountController.java
@@ -3,17 +3,34 @@
  */
 package com.cmc.controller;
 
+import com.cmc.PsuedoDatabase;
+import com.cmc.model.Account;
+
 /**
  * @author wench
  *
  */
 public class AccountController {
+	
+	private static AccountController self;
 
 	/**
 	 * 
 	 */
-	public AccountController() {
+	private AccountController() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static AccountController getInstance() {
+		if (self == null) self = new AccountController();
+		return self;
+	}
+	
+	public Account logon(String username, String password) {
+		PsuedoDatabase db = new PsuedoDatabase();
+		Account user = db.getUserbyUsername(username);
+		if (user.logon(password)) return user;
+		return null;
 	}
 
 }
