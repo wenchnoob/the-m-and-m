3a636080ec29c6b3ad7461d16926f848fababbe4
ZKRATZ001 <zkratz001@vplincsci013.ad.csbsju.edu>
2021-03-09 11:43:38 -0600

    zach getting things together

--- src/com/cmc/database/DBInteractions.java
+++ src/com/cmc/database/DBInteractions.java
@@ -4,8 +4,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import com.cmc.model.*;
-import csb.sju.csci.*;
-//import dblibrary.project.csci230.UniversityDBLibrary;
+//import csb.sju.csci.*;
+import dblibrary.project.csci230.UniversityDBLibrary;
 
 public class DBInteractions {
 	
@@ -16,10 +16,10 @@ public class DBInteractions {
 		// Initializer for All
 		// Uncomment the below line if you are in horizon view.
 
-		//db = new UniversityDBLibrary("megatherium", "csci230");
+		db = new UniversityDBLibrary("megatherium", "csci230");
 		
 		// Initializer for Wenchy (Comment out if you are not wenchy)
-		db = new UniversityDBLibrary("jdbc:mysql://localhost:3306/megatherium", "cmc", "pleasejustwork!");
+		//db = new UniversityDBLibrary("jdbc:mysql://localhost:3306/megatherium", "cmc", "pleasejustwork!");
 	}
 	
 	public static DBInteractions getInstance() {
