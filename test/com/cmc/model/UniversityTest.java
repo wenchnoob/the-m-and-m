892dff8dd0c0bcf16876bfbb6e323bb481d674be
Joseph Mathias <jmathias001@vplincsci077.ad.csbsju.edu>
2021-03-10 22:49:00 -0600

    University Test

--- test/com/cmc/model/UniversityTest.java
+++ test/com/cmc/model/UniversityTest.java
@@ -1,19 +1,57 @@
 package com.cmc.model;
 
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cmc.database.DBInteractions;
+
 import junit.framework.TestCase;
 
 public class UniversityTest extends TestCase {
+	
+	private DBInteractions db;
+	private University testUniversity;
+	ArrayList<String> emphases = new ArrayList<String>();
+	
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		db = DBInteractions.getInstance();
+		emphases.add("String");
+		testUniversity = new University("SCHOOL", "STATE", "LOCATION", "CONTROL", 1, 1, 1, 1, 1, 1, 
+				1, emphases, 1.0f, 1.0f, 1.0f, 1.0f, 1);
+		
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
+		db = null;
+		emphases = null;
+		testUniversity = null;
 	}
-
-	public void testUniversity() {
-		fail("Not yet implemented");
+	
+	@Test
+	public void testUniversityObjectInitilization() {
+		Assert.assertEquals("tests to make sure name is initialized", testUniversity.getName(), "SCHOOL");
+		Assert.assertEquals("tests to make sure state is initialized", testUniversity.getState(), "STATE");
+		Assert.assertEquals("tests to make sure location is initialized",testUniversity.getLocation(), "LOCATION");
+		Assert.assertEquals("tests to make sure control is initialized", testUniversity.getControl(), "CONTROL");
+		Assert.assertEquals("tests to make sure numStudents is initialized", testUniversity.getNumStudents(), 1);
+		Assert.assertEquals("tests to make sure satMath is initialized", testUniversity.getSatMath(), 1);
+		Assert.assertEquals("tests to make sure satVerbal is initialized", testUniversity.getSatVerbal(), 1);
+		Assert.assertEquals("tests to make sure expenses is initialized", testUniversity.getExpenses(), 1);
+		Assert.assertEquals("tests to make sure numOfApps is initialized", testUniversity.getNumOfApps(), 1);
+		Assert.assertEquals("tests to make sure academicScale is initialized", testUniversity.getAcademicScale(), 1);
+		Assert.assertEquals("tests to make sure is socialScale initialized", testUniversity.getSocialScale(), 1);
+		Assert.assertEquals("tests to make sure is emphases initialized", testUniversity.getEmphases(), emphases);
+		Assert.assertTrue("tests to make sure is perFemale initialized", Double.valueOf(testUniversity.getPerFemale()) == 1.0);
+		Assert.assertTrue("tests to make sure is perAdmitted initialized", Double.valueOf(testUniversity.getPerAdmitted()) == 1.0);
+		Assert.assertTrue("tests to make sure is perFinAid initialized", Double.valueOf(testUniversity.getPerFinAid()) == 1.0);
+		Assert.assertTrue("tests to make sure is perEnrolled initialized", Double.valueOf(testUniversity.getPerEnrolled()) == 1.0);
+		Assert.assertEquals("tests to make sure is qualityLife initialized", testUniversity.getQualityLife(), 1);
+		
 	}
 
 }
